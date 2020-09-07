cd043e685b41241b1c2f96244d38bf9b26b90a46
jacobwitflem@gmail.com <jacobwitflem@gmail.com>
2020-09-07 22:40:36 +0300

    added admin-edit functionality -jacob

--- main.py
+++
@@ -1,2 +0,0 @@
-print("Sex")
-print("SEX")
--- myproject/myproject/apps/polls/models.py
+++ myproject/myproject/apps/polls/models.py
@@ -1,10 +1,22 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
-	qestion_text = models.CharField('question text', max_length = 200)
+	question_text = models.CharField('question text', max_length = 200)
 	pub_date = models.DateTimeField('publication date')
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+	def __str__(self):
+		return self.question_text
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete = models.CASCADE)
 	choice_text = models.CharField('choice text', max_length = 200)
 	votes = models.IntegerField(default = 0)
+
+	def __str__(self):
+		return self.choice_text
